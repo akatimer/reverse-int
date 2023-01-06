0da34949c864022daab1c82f03f654c45ae434bc
akatimer <akatimer@gmail.com>
2023-01-06 21:21:58 +0200

    feat: code added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function reverse (n) {
-  
+  let new_n=0;
+  let s =0;
+  n=Math.abs(n)
+  if (Math.floor(n/10) > 0 ) {
+      s = s+1
+      if (Math.floor(n/100) > 0) {
+        s= s+1
+        if (Math.floor(n/1000) > 0) {
+          s=s+1
+          if (Math.floor(n/10000) > 0){
+            s=s+1
+            if (Math.floor(n/100000) >0) {
+              s=s+1
+            }
+          }
+        }
+      }
+  }
+ console.log(s)
+ let i=0;
+ for ( s  ; s>-1 ; s-1) {
+  new_n = new_n + (10**i)*Math.floor(n/10**s)
+  console.log ((10**i)*Math.floor(n/10**s))
+  n=Math.floor(n%10**s)
+  s=s-1;
+  i=i+1;
+ } 
+ return new_n
 }
